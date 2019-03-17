63bd460dca6eab60449b156680ef209af0c38a44
volynsky <vlnsk@ukr.net>
2019-03-17 14:54:30 +0200

    Added new section

--- options-page.php
+++ options-page.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * Output all fields of a options page.
+ */
 function vlnsk_option_page_output () {
     ?>
     <div class="wrap">
@@ -15,21 +17,55 @@ function vlnsk_option_page_output () {
     <?php
 }
 
+/**
+ * First section description.
+ */
 function vlnsk_first_section_callback() {
     echo 'First section description';
 }
 
+/**
+ * Create first option field.
+ *
+ * @param $option_props
+ */
 function vlnsk_first_option_callback( $option_props ) {
     $option_value = get_option( $option_props['option_name'] ) or '';
 
     printf(
-        '<input class="text" id="%s" name="%s" type="text" value="%s" placeholder="First option value" />',
+        '<input class="regular-text" id="%s" name="%s" type="text" value="%s" placeholder="First option value" />',
         esc_attr( $option_props['id'] ),
         esc_attr( $option_props['option_name'] ),
         esc_attr( $option_value )
     );
 }
 
+/**
+ * Description for second section.
+ */
+function vlnsk_second_section_callback() {
+	echo 'Second section description';
+}
+
+/**
+ * Create second option field.
+ *
+ * @param $option_props
+ */
+function vlnsk_second_option_callback( $option_props ) {
+	$option_value = get_option( $option_props['option_name'] ) or '';
+
+	printf(
+		'<textarea class="large-text" id="%s" name="%s" type="text" placeholder="Second option value">%s</textarea>',
+		esc_attr( $option_props['id'] ),
+		esc_attr( $option_props['option_name'] ),
+		esc_attr( $option_value )
+	);
+}
+
+/**
+ * Add new admin menu item.
+ */
 function vlnsk_option_page_menu() {
     add_menu_page(
         'My options', //change this
@@ -42,6 +78,9 @@ function vlnsk_option_page_menu() {
     );
 }
 
+/**
+ * Register all options fields.
+ */
 function vlnsk_option_page_register_settings() {
     register_setting(
         'vlnsk_options',
@@ -50,6 +89,14 @@ function vlnsk_option_page_register_settings() {
             'sanitize_callback' => 'sanitize_text_field'
         )
     );
+
+	register_setting(
+		'vlnsk_options',
+		'second_option',
+		array(
+			'sanitize_callback' => 'sanitize_textarea_field'
+		)
+	);
 
     add_settings_section(
         'first_section',
@@ -69,6 +116,25 @@ function vlnsk_option_page_register_settings() {
             'option_name'   => 'first_option',
         )
     );
+
+	add_settings_section(
+		'second_section',
+		'Second section title',
+		'vlnsk_second_section_callback',
+		'vlnsk_options'
+	);
+
+	add_settings_field(
+		'second_option',
+		'Second option title',
+		'vlnsk_second_option_callback',
+		'vlnsk_options',
+		'second_section',
+		array(
+			'id'            => 'second-option',
+			'option_name'   => 'second_option',
+		)
+	);
 }
 
 if ( is_admin() ) {
