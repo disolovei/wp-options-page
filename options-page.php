5085fcb8bf8ee12468fd2ea7610f2e08f8b6ac6a
Dima Solovey <vlnsk@ukr.net>
2019-09-13 08:21:01 +0300

    New version

--- options-page.php
+++ options-page.php
@@ -1,143 +1,68 @@
 <?php
 /**
- * Output all fields of a options page.
+ * 
  */
-function vlnsk_option_page_output () {
+function vlnsk_output_option_page() {
+    $active_tab = ! empty( $_GET['tab'] ) ? strip_tags( $_GET['tab'] ) : 'general';
     ?>
+
     <div class="wrap">
-        <h1><?php echo get_admin_page_title(); ?></h1>
-        <form action="options.php" method="POST">
-            <?php
-            settings_fields( 'vlnsk_options' );
-            do_settings_sections( 'vlnsk_options' );
-            submit_button( 'Save options' );
-            ?>
-        </form>
+        <h2><?php echo get_admin_page_title(); ?></h2>
     </div>
+    <p class="nav-tab-wrapper">
+		<a href="?page=custom_option_page_slug&tab=general" class="nav-tab<?php echo 'general' == $active_tab ?  ' nav-tab-active': ''; ?>">General section</a>
+		<a href="?page=custom_option_page_slug&tab=second" class="nav-tab<?php echo 'second' == $active_tab ?  ' nav-tab-active': ''; ?>">Second section</a>
+	</h2>
+
+    <?php settings_errors(); ?>
+
+    <form method="post" action="options.php">
+
+		<?php
+			if ( 'general' == $active_tab ) {
+				settings_fields( 'rvr_profile_settings_section' );
+				do_settings_sections( 'rvr_profile_settings' );
+			} else {
+				settings_fields( 'rvr_primary_settings_section' );
+				do_settings_sections( 'rvr_primary_settings' );
+			}
+			submit_button();
+			?>
+
+	</form>
+
     <?php
 }
 
 /**
- * First section description.
+ * 
  */
-function vlnsk_first_section_callback() {
-    echo 'First section description';
+function vlnsk_add_option_page() {
+    add_submenu_page( 
+        'options-general.php', 
+        'Custom options page', 
+        'Custom options page', 
+        'manage_options', 
+        'custom_option_page_slug', 
+        'vlnsk_output_option_page'
+     );
+}
+add_action( 'admin_menu', 'vlnsk_add_option_page' );
+
+/**
+ * 
+ */
+function vlnsk_first_section_desc() {
+    echo '<p>First section description</p>';
 }
 
 /**
- * Create first option field.
- *
- * @param $option_props
+ * 
  */
-function vlnsk_first_option_callback( $option_props ) {
-    $option_value = get_option( $option_props['option_name'] ) or '';
-
-    printf(
-        '<input class="regular-text" id="%s" name="%s" type="text" value="%s" placeholder="First option value" />',
-        esc_attr( $option_props['id'] ),
-        esc_attr( $option_props['option_name'] ),
-        esc_attr( $option_value )
-    );
+function vlnsk_create_options() {
+    // add_settings_section( 'vlnsk_first_section', 'First section title', null, 'options' );
+    // or
+    add_settings_section( 'vlnsk_first_section', 'First section title', 'vlnsk_first_section_desc', 'options' );
+    add_settings_section( 'vlnsk_second_section', 'Second section title', 'vlnsk_second_section_desc', 'options' );
 }
-
-/**
- * Description for second section.
- */
-function vlnsk_second_section_callback() {
-	echo 'Second section description';
-}
-
-/**
- * Create second option field.
- *
- * @param $option_props
- */
-function vlnsk_second_option_callback( $option_props ) {
-	$option_value = get_option( $option_props['option_name'] ) or '';
-
-	printf(
-		'<textarea class="large-text" id="%s" name="%s" type="text" placeholder="Second option value">%s</textarea>',
-		esc_attr( $option_props['id'] ),
-		esc_attr( $option_props['option_name'] ),
-		esc_attr( $option_value )
-	);
-}
-
-/**
- * Add new admin menu item.
- */
-function vlnsk_option_page_menu() {
-    add_menu_page(
-        'My options', //change this
-        'My options', //change this
-        'manage_options',
-        'vlnsk_options', //change this
-        'vlnsk_option_page_output',
-        'dashicons-admin-generic',
-        101
-    );
-}
-
-/**
- * Register all options fields.
- */
-function vlnsk_option_page_register_settings() {
-    register_setting(
-        'vlnsk_options',
-        'first_option',
-        array(
-            'sanitize_callback' => 'sanitize_text_field'
-        )
-    );
-
-	register_setting(
-		'vlnsk_options',
-		'second_option',
-		array(
-			'sanitize_callback' => 'sanitize_textarea_field'
-		)
-	);
-
-    add_settings_section(
-        'first_section',
-        'First section title',
-        'vlnsk_first_section_callback',
-        'vlnsk_options'
-    );
-
-    add_settings_field(
-        'first_option',
-        'First option title',
-        'vlnsk_first_option_callback',
-        'vlnsk_options',
-        'first_section',
-        array(
-            'id'            => 'first-option',
-            'option_name'   => 'first_option',
-        )
-    );
-
-	add_settings_section(
-		'second_section',
-		'Second section title',
-		'vlnsk_second_section_callback',
-		'vlnsk_options'
-	);
-
-	add_settings_field(
-		'second_option',
-		'Second option title',
-		'vlnsk_second_option_callback',
-		'vlnsk_options',
-		'second_section',
-		array(
-			'id'            => 'second-option',
-			'option_name'   => 'second_option',
-		)
-	);
-}
-
-if ( is_admin() ) {
-    add_action( 'admin_menu', 'vlnsk_option_page_menu' );
-    add_action( 'admin_init', 'vlnsk_option_page_register_settings' );
-}
+add_action( 'admin_init', 'vlnsk_create_options' );
